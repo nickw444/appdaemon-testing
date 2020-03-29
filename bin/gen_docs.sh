00a0497176aa94cba7d284d7d15b18b2f3688e37
Nick Whyte <nick@nickwhyte.com>
2020-03-29 21:10:21 +1100

    .

--- bin/gen_docs.sh
+++ bin/gen_docs.sh
@@ -6,7 +6,9 @@ REPO_DIR="$(git rev-parse --show-toplevel)"
 main() {
   cd "${REPO_DIR}";
   rm -rf docs;
-  pdoc --html --force -o docs  --filter 'pytest,HassDriver'  appdaemon_testing/;
+  mkdir -p target
+  pdoc --html --force -o target/docs  --filter 'pytest,HassDriver'  appdaemon_testing/;
+  mv target/docs/appdaemon_testing docs/
 }
 
 main "$@"
